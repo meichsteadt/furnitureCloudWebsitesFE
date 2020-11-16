b0c8628c0fa6ad833735627bcb489f830b04f4d1
Matt Eichsteadt <meichsteadt@jobcase.com>
2020-11-16 11:19:03 -0500

    remove HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
 
 import { routing } from './app.routing';
 
